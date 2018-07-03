998981a520434778bcfe46bfb5afbc000298d316
Paulo Barbosa <paulo.n.barbosa@hotmail.com>
2018-07-02 22:01:32 -0300

    TDD with rspec part 1/2

---
+++ spec/calculator/calculator_spec.rb
@@ -0,0 +1,9 @@
+require 'calculator'
+
+describe Calculator do
+  it "use sum method for 2 numbers" do
+    calc = Calculator.new
+    result = calc.sum(5, 7)
+    expect(result).to eq 12
+  end
+end
