e4d1159804755ac331ce038e9db24f32b8147792
Paulo Barbosa <paulo.n.barbosa@hotmail.com>
2018-07-06 20:25:33 -0300

    matchers true and false

--- spec/matchers/comparison/comparison_spec.rb
+++ spec/matchers/comparison/comparison_spec.rb
@@ -1,26 +1,26 @@
-describe 'Matchers de Comparação' do
-  it '#equal - Testa objetos' do
+describe 'Matchers of comparison' do
+  it '#equal - Test objects' do
     x = 'ruby'
     y = 'ruby'
 
     expect(x).not_to equal y
   end
 
-  it '#be - Testa objetos' do
+  it '#be - Test objects' do
     x = 'ruby'
     y = 'ruby'
 
     expect(x).not_to be y
   end
 
-  it '#eql - Testa o valor/conteúdo' do
+  it '#eql - Test values' do
     x = 'ruby'
     y = 'ruby'
 
     expect(x).to eql y
   end
 
-  it '#eq - Testa o valor/conteúdo' do
+  it '#eq - Test values' do
     x = 'ruby'
     y = 'ruby'
 
