a7b241e478107035fe9ba40398ce9717c333269b
Paulo Barbosa <paulo.n.barbosa@hotmail.com>
2018-07-22 17:58:31 -0300

    as null object

--- spec/test_doubles/user_spec.rb
+++ spec/test_doubles/user_spec.rb
@@ -4,4 +4,13 @@ describe "Test Double" do
     allow(user).to receive_messages(name: 'Jack', password: 'secret')
     puts user.inspect
   end
+
+  it 'as_null_object' do
+    user = double('User').as_null_object
+    allow(user).to receive(:name).and_return('Jack')
+    allow(user).to receive(:password).and_return('secret')
+    puts user.name
+    puts user.password
+    puts user.abc
+  end
 end
